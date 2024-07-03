1015b16035bb4c61567385a05058a5d1aba8ada8
Toon <159065167+toondark@users.noreply.github.com>
2024-07-02 18:32:18 -0600

    Update index.js
    Solución a problemas de la prueba técnica.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,51 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; // Módulo de importación desde API
+var A = 'https://rickandmortyapi.com/api/character/'; // URL de la API
 
-var A = 'https://rickandmortyapi.com/api/character/';
-var B = new XMLHttpRequest();
+// Función para realizar la solicitud HTTP usando Promesas.
+const X = (a) => {
+  return new Promise((resolve, reject) => { //Se retorna la promesa
+    var B = new XMLHttpRequest(); // Se declara dentro de la función la variable B que crea la instancia de solicitud ya que esta debe ciclarse a cada solicitud y no solo una vez.
+    
+    B.onreadystatechange = (e) => {  
+      if (B.readyState == 4) { // Códigos HTML no deben ser cadenas de texto, son números.
+        if (B.status === 200) {
+          resolve(B.responseText); // Resuelve la promesa con response.
+        } else {
+          reject(new Error(`Imposible obtener la información ${a}`)); // Rechaza la promesa con un error.
+        }
+      }
+    };
 
-function X(a, b) {
-  B.onreadystatechange = function (e) {
-    if (B.readyState == '4') {
-      if (B.status === '200')
-        b(null, B.responseText);
-      else return b(a);
-    }
-    else return b(a);
-  };
-  B.open('GET', a, false);
-  B.send();
+    B.open('GET', a, true); // Se maneja de forma asincrónica cambiando true por false.
+    B.send();
+  });
+}
+
+// Funcion que sustituye el callback hell. async
+const main = async () => {
+  try {
+    // Primera llamada a la API para obtener la lista de personajes.
+    const d = await X(A); //Espera resolucion de promesa con la URL
+    console.log('Primer Llamado...');
+    var results = JSON.parse(d).results; // Convertimos la propiedad results del JSON en un objeto de JS 
+
+    // Segunda llamada para obtener información del primer personaje.
+    const f = await X(A + results[0].id); // Espera de promesa combinando el request con el objeto creado.
+    console.log('Segundo Llamado...');
+    var char = JSON.parse(f); // Convertimos en objeto el JSON del request f.
+
+    // Tercera llamada para obtener la información del origen del primer personaje.
+    const h = await X(char.origin.url); //Nuevamente espera resolucion de promesa 
+    console.log('Tercer Llamado...');
+    var origin = JSON.parse(h); // Otro parseo del JSON. 
+
+    // Muestra la información obtenida.
+    console.log(`Personajes: ${JSON.parse(d).info.count}`);
+    console.log(`Primer Personaje: ${char.name}`);
+    console.log(`Dimensión: ${origin.dimension}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
 };
 
-X(A, function (c, d) {
-  if (c) return console.error('Error' + ' ' + c);
-  console.log('Primer Llamado...');
-  X(A + d.results[0].id, function (e, f) {
-    if (e) return console.error('Error' + ' ' + e);
-    console.log('Segundo Llamado...');
-    X(JSON.parse(f).origin.url, function (g, h) {
-      if (g) return console.error('Error' + ' ' + g);
-      console.log('Tercer Llamado...');
-      console.log('Personajes:' + ' ' + JSON.parse(d).info.count);
-      console.log('Primer Personaje:' + ' ' + JSON.parse(f).name);
-      console.log('Dimensión:' + ' ' + JSON.parse(h).dimension);
-    });
-  });
-});
+main(); // Por ultimo no olvidar invocar la funcion main.
